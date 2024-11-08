e6f185223b96b53ac8dc4a47ae61581b75c8b5ab
Md Tanvir Sarwar <ms24adn@herts.ac.uk>
2024-11-08 23:36:52 +0000

    Introduction to Pointer and Linked List

--- linked list.cpp
+++ linked list.cpp
@@ -9,7 +9,7 @@ struct node{
     Node *next;
 };
 
-Node *create_node(int item, Node *next)
+Node* create_node(int item, Node *next)
 {
     Node *new_node = (Node *) malloc(sizeof(Node));
     if(new_node == NULL)
