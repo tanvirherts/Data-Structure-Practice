7937d2ba9afaba69e8de3e566b0bfd4332654b1f
Md Tanvir Sarwar <ms24adn@herts.ac.uk>
2024-11-09 14:29:02 +0000

    Practice for traversal of linked list

---
+++ Practice/traverse.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+public:
+
+    int data;
+    Node* next;
+
+    Node(int new_data)
+    {
+        this->data = new_data;
+        this->next = nullptr;
+    }
+};
+
+
+int main()
+{
+    
+}
