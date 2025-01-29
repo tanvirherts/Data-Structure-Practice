311787d2787e8127d45a5d82b20996a3e1d6764f
Md Tanvir Sarwar <ms24adn@herts.ac.uk>
2025-01-29 16:48:57 +0000

    Problem solving in Codeforces

--- Practice/traverse.cpp
+++ Practice/traverse.cpp
@@ -15,8 +15,31 @@ public:
     }
 };
 
+void traverseLinkedList(Node* head)
+{
+    Node* current = head;
+    
+    while(current!=nullptr)
+    {
+        cout<<current->data<<" ";
+        current = current->next;
+    }
+    if(current == nullptr)
+    {
+        cout<<endl;
+        return;
+    }
+
+}
 
 int main()
 {
-    
+    Node* head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+    head->next->next->next->next = new Node(50);
+
+    traverseLinkedList(head);
+
 }
